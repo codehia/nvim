3557eb78d55a1636ce6e4151d3156ae69c944950
Soumya Acharya <codehia@pop-os.localdomain>
2021-05-13 22:30:37 +0530

    add basic plugins for lsp

---
+++ lua/plugins.lua
@@ -0,0 +1,62 @@
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+
+if fn.empty(fn.glob(install_path)) > 0 then
+    execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+    execute "packadd packer.nvim"
+end
+
+--- Check if a file or directory exists in this path
+local function require_plugin(plugin)
+    local plugin_prefix = fn.stdpath("data") .. "/site/pack/packer/opt/"
+
+    local plugin_path = plugin_prefix .. plugin .. "/"
+    --	print('test '..plugin_path)
+    local ok, err, code = os.rename(plugin_path, plugin_path)
+    if not ok then
+        if code == 13 then
+            -- Permission denied, but it exists
+            return true
+        end
+    end
+    --	print(ok, err, code)
+    if ok then
+        vim.cmd("packadd " .. plugin)
+    end
+    return ok, err, code
+end
+
+vim.cmd "autocmd BufWritePost plugins.lua PackerCompile" -- Auto compile when there are changes in plugins.lua
+
+return require("packer").startup(
+    function(use)
+        -- Packer can manage itself as an optional plugin
+        use "wbthomason/packer.nvim"
+
+        -- TODO refactor all of this (for now it works, but yes I know it could be wrapped in a simpler function)
+        use {"neovim/nvim-lspconfig", opt = true}
+        use {"glepnir/lspsaga.nvim", opt = true}
+        use {"kabouzeid/nvim-lspinstall", opt = true}
+
+        -- Autocomplete
+        use {"hrsh7th/nvim-compe", opt = true}
+        use {"hrsh7th/vim-vsnip", opt = true}
+        use {"rafamadriz/friendly-snippets", opt = true}
+
+        -- Treesitter
+        use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
+        use {"windwp/nvim-ts-autotag", opt = true}
+
+
+        require_plugin("nvim-lspconfig")
+        require_plugin("lspsaga.nvim")
+        require_plugin("nvim-lspinstall")
+        require_plugin("friendly-snippets")
+        require_plugin("nvim-compe")
+        require_plugin("vim-vsnip")
+        require_plugin("nvim-treesitter")
+        require_plugin("nvim-ts-autotag")
+    end
+)
